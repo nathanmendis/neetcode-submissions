50a3205ecd7fe89e8f1ba88a916eebb60e70b33c
Nathan Mendis <124086542+nathanmendis@users.noreply.github.com>
2026-08-04 17:18:37 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+       ## return s == s[::-1]
+        clean=""
+        for ch in s:
+            if ch.isalnum():
+                clean+=ch.lower()
+        left=0
+        right=len(clean)-1
+        while left<right:
+            if clean[left]!=clean[right]:
+                return False
+            left+=1
+            right-=1
+        return True
+
